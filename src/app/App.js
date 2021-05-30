cbc175b6f5c30610b04dc3b8124aef73951c977f
Aziz Mohamed <aziz@azizmohamed.com>
2021-05-30 20:50:56 +1000

    add page title

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import logo from '../logo.svg';
 import './App.css';
 import Todos from './components/todoList/todos.js';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+          <title>Todo List</title>
+      </Helmet>
       <header className="App-header">
         Todo List
       </header>
